6bc2ae55e2dc92acc21f08f9b61c5ee8f3b4b82a
mabrams <mabrams@uab.edu>
2016-11-29 13:27:08 -0600

    initial update: added core.matrix, incanter, etc.

--- project.clj
+++ project.clj
@@ -1,9 +1,14 @@
 (defproject free-agent "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :description "Agent-based simulation with free energy minimization within agents."
+  :url "https://github.com/mars0i/free-agent"
+  :license {:name "Gnu General Public License version 3.0"
+            :url "http://www.gnu.org/copyleft/gpl.html"}
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+                 [net.mikera/core.matrix "0.57.0"]
+                 [net.mikera/vectorz-clj "0.45.0"]
+                 [org.clojure/math.numeric-tower "0.0.4"]
+                 [incanter "1.5.7"]
+                 [criterium "0.4.4"]] ; to use, e.g.: (use '[criterium.core :as c])
   :main ^:skip-aot free-agent.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
