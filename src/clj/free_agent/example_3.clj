10b03d2d1fae9d237d84d4affc3c7b55874f9427
mars0i <mabrams@uab.edu>
2016-11-29 21:42:10 -0600

    replaced arithmetic require with free-agent.matrix

--- src/clj/free_agent/example_3.clj
+++ src/clj/free_agent/example_3.clj
@@ -6,37 +6,37 @@
 
 (ns free-agent.example-3
   (:require [free-agent.level :as lvl]
-            [clojure.core.matrix :as mx] ; needed for arithmetic macros even if not used explicitly
+            [clojure.core.matrix :as m] ; needed for arithmetic macros even if not used explicitly
             [free.random :as ran] ; clj or cljs depending on dialect
-            [free.arithmetic :as ar]))
+            [free-agent.matrix :as fm]))
 
-(mx/set-current-implementation :vectorz)
+(m/set-current-implementation :vectorz)
 
 ;; Since these next three functions run on every tick, maybe slightly
 ;; faster not to use ar/col-mat:
 
-(defn gen [phi] (let [x1 (mx/mget phi 0 0)
-                      x2 (mx/mget phi 1 0)]
-                  (mx/matrix [[(* x1 x1 x2)]
+(defn gen [phi] (let [x1 (m/mget phi 0 0)
+                      x2 (m/mget phi 1 0)]
+                  (m/matrix [[(* x1 x1 x2)]
                               [(* x2 x2 x1)]])))
 
-(defn gen' [phi] (let [x1 (mx/mget phi 0 0)
-                       x2 (mx/mget phi 1 0)]
-                   (mx/matrix [[(* x2 2.0 x1)]
+(defn gen' [phi] (let [x1 (m/mget phi 0 0)
+                       x2 (m/mget phi 1 0)]
+                   (m/matrix [[(* x2 2.0 x1)]
                                [(* x1 2.0 x2)]])))
 
 (def next-bottom (lvl/make-next-bottom 
-                   #(mx/matrix [[(ran/next-gaussian  2 5)]
+                   #(m/matrix [[(ran/next-gaussian  2 5)]
                                 [(ran/next-gaussian -1 3)]])))
 
-(def init-theta (ar/make-identity-obj 2)) ; i.e. initially pass value of gen(phi) through unchanged
+(def init-theta (m/identity-matrix 2)) ; i.e. initially pass value of gen(phi) through unchanged
 
 ; what phi is initialized to, and prior mean at top:
-(def v-p (ar/col-mat [3.0 3.0]))
+(def v-p (fm/col-mat [3.0 3.0]))
 
-(def bot-map {:phi   (ar/col-mat [0.0 0.0]) ; immediately replaced by next-bottom
-              :epsilon   (ar/col-mat [0.0 0.0])
-              :sigma (mx/matrix [[2.0  0.25]  ; it's a covariance matrix, so
+(def bot-map {:phi   (fm/col-mat [0.0 0.0]) ; immediately replaced by next-bottom
+              :epsilon   (fm/col-mat [0.0 0.0])
+              :sigma (m/matrix [[2.0  0.25]  ; it's a covariance matrix, so
                                  [0.25 2.0]]) ; should be symmetric
               :theta init-theta
               :gen  nil
@@ -47,15 +47,15 @@
               :theta-dt 0.0})
 
 (def mid-map {:phi v-p
-              :epsilon   (ar/col-mat [0.0 0.0])
-              :sigma (mx/matrix [[2.0  0.25]
-                                 [0.25 2.0]])
+              :epsilon (fm/col-mat [0.0 0.0])
+              :sigma (m/matrix [[2.0  0.25]
+                                [0.25 2.0]])
               :theta init-theta
               :gen  gen
               :gen' gen'
-              :phi-dt    0.0001
-              :epsilon-dt    0.01
-              :sigma-dt  0.0001
+              :phi-dt 0.0001
+              :epsilon-dt 0.01
+              :sigma-dt 0.0001
               :theta-dt 0.01})
 
 (def init-bot (lvl/map->Level bot-map))
