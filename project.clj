b35f00b5660d09cc9f7e5ccbd066840c246e8493
mars0i <mabrams@uab.edu>
2016-11-30 11:12:06 -0600

    Added MASON config

--- project.clj
+++ project.clj
@@ -8,10 +8,20 @@
                  [net.mikera/vectorz-clj "0.45.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]
                  [incanter "1.5.7"]
-                 [criterium "0.4.4"]] ; to use, e.g.: (use '[criterium.core :as c])
+                 [criterium "0.4.4"] ; to use, e.g.: (use '[criterium.core :as c])
+                 [org.jfree/jcommon "1.0.21"]    ; for MASON
+                 [org.jfree/jfreechart "1.0.17"] ; for MASON
+                 [org.beanshell/bsh "2.0b4"]]    ; for MASON
 
+  :resource-paths ["resources/jars"]
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"]
   :main ^:skip-aot free-agent.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
+
+
+  ; jvm-opts ["-Xms1g"]
+  ;:jvm-opts ["-Dclojure.compiler.disable-locals-clearing=true"] ; ???: FASTER, and may be useful to debuggers. see https://groups.google.com/forum/#!msg/clojure/8a1FjNvh-ZQ/DzqDz4oKMj0J
+  ;:jvm-opts ["-XX:+TieredCompilation" "-XX:TieredStopAtLevel=1"] ; setting this to 1 will produce faster startup but will disable extra optimization of long-running processes
+  ;:jvm-opts ["-XX:TieredStopAtLevel=4"] ; more optimization (?)
