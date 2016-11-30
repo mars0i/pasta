fb4ebaeaa7f36a8335b0aaeafb3b82b2bb834abd
mars0i <mabrams@uab.edu>
2016-11-29 21:50:02 -0600

    added rng def

--- src/clj/free_agent/core.clj
+++ src/clj/free_agent/core.clj
@@ -1,6 +1,10 @@
 (ns free-agent.core
   (:gen-class))
 
+(def random-seed (ran/make-long-seed))
+(def rng (ran/make-rng random-seed))
+(println "random-seed:" random-seed)
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
