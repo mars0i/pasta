22bc87060ef2182c4fb748288e1db29270def31e
mabrams <mabrams@uab.edu>
2016-12-07 13:12:26 -0600

    defsimstate now includes defrecord and rudimentary gen-class

--- src/clj/utils/defsimstate.clj
+++ src/clj/utils/defsimstate.clj
@@ -34,3 +34,20 @@
   `(symbol 
      (str ~prefix 
           (hyphed-to-studly-str (name '~sym)))))
+
+(defmacro defsimstate
+  "Requires option :field followed by a vector of field names, and :name for the name of 
+  the class to be defined by gen-class."
+  [& opts-seq]
+   (let [opts (into {} (map vec (partition 2 opts-seq)))
+         fields (:fields opts)   ; take out my special entries
+         opts (dissoc opts :state-vars)  ; leave only entries gen-class knows
+         opts (assoc opts                ; add in gen-class options we want for sure
+                     :state 'instanceState
+                     :exposes-methods '{start superStart}
+                     :init 'init-instance-state
+                     :main true)]
+     `(do
+        (defrecord ~'InstanceState ~fields) ; TODO make sure InstanceState comes out in the right namespace
+        (gen-class ~@(apply concat opts)))))
+
