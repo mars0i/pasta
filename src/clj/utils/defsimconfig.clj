533337e26343764d8dc158b1dc2a4288da6cb3c5
mars0i <mabrams@uab.edu>
2016-12-09 12:18:27 -0600

    fixed sym-str bug, abstracted names, finished dom method specs

--- src/clj/utils/defsimconfig.clj
+++ src/clj/utils/defsimconfig.clj
@@ -4,11 +4,25 @@
 (ns utils.defsimconfig
   (:require [clojure.string :as s]))
 
+(def class-sym 'SimConfig)
+(def class-constructor 'SimConfig.)
+(def data-class-sym 'SimConfigData)
+(def data-sym 'simConfigData)
+(def data-accessor '.simConfigData)
+(def init-sym 'init-sim-config-data)
+
 (defn hyphed-to-studly-str
   "Converts a hyphenated string into the corresponding studly caps string."
-  [hyphed-str]
-  (let [parts (s/split hyphed-str #"-")]
+  [string]
+  (let [parts (s/split string #"-")]
     (reduce str (map s/capitalize parts))))
+
+(defn hyphed-sym-to-studly-str
+  "Convience wrapper for hyphed-to-studly-str that converts symbol to
+  string before calling it."
+  [sym]
+  (hyphed-to-studly-str (name sym)))
+
 
 (defn make-accessor-sym
   "Given a prefix string and a Clojure symbol, returns a Java 
@@ -22,6 +36,14 @@
   (mapcat (fn [get-sym set-sym cls] [[get-sym [] cls] [set-sym [cls] 'void]])
                get-syms set-syms classes))
 
+(defn range-fields
+  "Given a fields argument to defsimconfig, return a sequence containing 
+  only those field specifications that include specification of the default
+  range of values for the field."
+  [fields]
+  (filter #(= 4 (count %)) 
+          fields))
+
 (defmacro defsimconfig
   "sim-state-class is a fully-qualified name for the new class. fields is a
   sequence of 2- or 4-element sequences starting with names of fields in which 
@@ -30,47 +52,41 @@
   are the min and max values for the field.  The following gen-class options will be 
   automatically provided: :state, :exposes-methods, :init, :main, :methods.  
   Additional options can be provided in addl-gen-class-opts."
-  [sim-state-class fields & addl-gen-class-opts]
-   (let [field-syms (map first fields)
+  [class-prefix fields & addl-gen-class-opts]
+   (let [qualified-class (symbol (str class-prefix "." class-sym))
+         field-syms (map first fields)
          field-keywords (map keyword field-syms)
          default-syms (map #(symbol (str "default-" %)) field-syms)
-         accessor-stubs (map hyphed-to-studly-str field-syms)
+         accessor-stubs (map hyphed-sym-to-studly-str field-syms)
          get-syms  (map (partial make-accessor-sym "get") accessor-stubs)
          set-syms  (map (partial make-accessor-sym "set") accessor-stubs)
          -get-syms (map (partial make-accessor-sym "-") get-syms)
          -set-syms (map (partial make-accessor-sym "-") set-syms)
-         dom-syms  (map (comp (partial make-accessor-sym "dom") hyphed-to-studly-str name first) ; TODO doesn't work
-                        (filter #(= 4 (count %)) fields))
-         ;(->> fields
-         ;     (filter #(= 4 (count %)))
-         ;     first
-         ;     name
-         ;     hyphed-to-studly-str
-         ;     (partial make-accessor-sym "dom"))
-         _ (println dom-syms)
-         gen-class-opts {:name sim-state-class
-                         :state 'configData
+         dom-syms  (map (comp (partial make-accessor-sym "dom") hyphed-sym-to-studly-str first)
+                        (range-fields fields))
+         gen-class-opts {:name qualified-class
+                         :state data-sym
                          :exposes-methods '{start superStart}
-                         :init 'init-config-data
+                         :init init-sym
                          :main true
                          :methods (vec (concat (make-accessor-sigs get-syms set-syms (map second fields))
-                                               (map #(vector % [] java.lang.Object) dom-syms)))} ;; TODO NOT WORKING
+                                               (map #(vector % [] java.lang.Object) dom-syms)))} 
          gen-class-opts (into gen-class-opts 
                               (map vec (partition 2 addl-gen-class-opts)))]
      `(do
-        ;;;; should be in a different namespace (so simulation code can access it without cyclicly referencing State):
-        (defrecord ~'ConfigData ~(vec field-syms)) ; TODO make sure ConfigData comes out in the right namespace
-        ;;;; should be in State namespace:
+        ;;;; should be in a different namespace (so simulation code can access it without cyclicly referencing SimConfig):
+        (defrecord ~data-class-sym ~(vec field-syms)) ; TODO make sure SimConfigData comes out in the right namespace
+        ;;;; should be in SimConfig namespace:
         (gen-class ~@(apply concat gen-class-opts))
         ;;;; Should be in same namespace as gen-class:
-        (defn ~'-init-config-data [~'seed] [[~'seed] (atom (ConfigData. ~@default-syms))]) ; NOTE will fail if default-syms are not yet defined.
+        (defn ~init-sym [~'seed] [[~'seed] (atom (~class-constructor ~@default-syms))]) ; NOTE will fail if default-syms are not yet defined.
         ;; need to add type annotations:
-        (import ~sim-state-class) ; must go after gen-class but before any type annotations using the class
-        ~@(map (fn [sym keyw] (list 'defn sym '[this] `(~keyw @(.configData ~'this))))
+        (import ~qualified-class) ; must go after gen-class but before any type annotations using the class
+        ~@(map (fn [sym keyw] (list 'defn sym '[this] `(~keyw @(.simConfigData ~'this))))
                -get-syms field-keywords)
-        ~@(map (fn [sym keyw] (list 'defn sym '[this newval] `(swap! (.configData ~'this) assoc ~keyw  ~'newval)))
+        ~@(map (fn [sym keyw] (list 'defn sym '[this newval] `(swap! (~data-accessor ~'this) assoc ~keyw  ~'newval)))
                -set-syms field-keywords)
-        ;; define domX for elements in domains
+        ;; TODO define domX for elements in domains
         )))
 
 
