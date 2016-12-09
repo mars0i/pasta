79562295fb3c8f334029189f23c67564ce2fbf4d
mars0i <mabrams@uab.edu>
2016-12-09 11:24:44 -0600

    switched to more informative names

--- src/clj/free_agent/SimConfig.clj
+++ src/clj/free_agent/SimConfig.clj
@@ -1,9 +1,9 @@
-(ns free-agent.Config
+(ns free-agent.SimConfig
   (:require [clojure.tools.cli])
   (:import [sim.engine Steppable Schedule]
            [ec.util MersenneTwisterFast]
            [java.lang String]))
-;; import free-agent.Config separately below
+;; import free-agent.SimConfig separately below
 ;; (if done here, fails when aot-compiling from a clean project)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -11,21 +11,21 @@
 
 ;; Initial defaults for global parameters
 (def default-snipe-energy 10.0)
-(def default-snipe-priors [5.0 20.0])
+(def default-r-snipe-priors [5.0 20.0])
 (def default-num-k-snipes 20)
 (def default-num-r-snipes default-num-k-snipes)
 
 
 ;; This will hold the global parameters in gen-class's single state field.
-(defrecord ConfigData [initial-snipe-energy
-                          snipe-priors
+(defrecord SimConfigData [initial-snipe-energy
+                          r-snipe-priors
                           num-k-snipes
                           num-r-snipes])
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; The SimState subclass specification
 
-(gen-class :name free-agent.Config
+(gen-class :name free-agent.SimConfig
            :extends sim.engine.SimState        ; includes signature for the start() method
            :exposes-methods {start superStart} ; alias method start() in superclass. (Don't name it 'super-start'; use a Java name.)
            ;; Bean methods that will be exposed to the UI--need to have Java types:
@@ -37,30 +37,30 @@
                      [getNumKSnipes [] long]
                      [setNumRSnipes [long] void]
                      [getNumRSnipes [] long]]
-           :state configData
-           :init init-config-data
+           :state simConfigData
+           :init init-sim-config-data
            :main true)
 
-;; TODO OH WAIT can I put the config-data somewhere else or if it's here there'll be cyclic deps .... ?
-(defn -init-config-data 
-  "Automatically initializes config-data when an instance of class Config is created."
+;; TODO OH WAIT can I put the sim-config-data somewhere else or if it's here there'll be cyclic deps .... ?
+(defn -init-sim-config-data 
+  "Automatically initializes sim-config-data when an instance of class SimConfig is created."
   [seed]
-  [[seed] (ConfigData. (atom default-snipe-energy)
-                       (atom default-snipe-priors)
+  [[seed] (SimConfigData. (atom default-snipe-energy)
+                       (atom default-r-snipe-priors)
                        (atom default-num-k-snipes)
                        (atom default-num-r-snipes))])
 
-(import free-agent.Config) ; do this after gen-class but before any type hints using Config
+(import free-agent.SimConfig) ; do this after gen-class but before any type hints using SimConfig
 
 ;; Bean accessors
-(defn -getInitialSnipeEnergy ^double [^Config this] @(:initial-snipe-energy ^ConfigData (.configData this)))
-(defn -setInitialSnipeEnergy [^Config this ^double newval] (reset! (:initial-snipe-energy ^ConfigData (.configData this)) newval))
-(defn -getInitialSnipePriors [^Config this] @(:initial-snipe-priors ^ConfigData (.configData this)))
-(defn -setInitialSnipePriors [^Config this newval] (reset! (:initial-snipe-priors ^ConfigData (.configData this) newval)))
-(defn -getNumKSnipes ^long [^Config this] @(:num-k-snipes ^ConfigData (.configData this)))
-(defn -setNumKSnipes [^Config this ^long newval] (reset! (:num-k-snipes ^ConfigData (.configData this)) newval))
-(defn -getNumRSnipes ^long [^Config this] @(:num-r-snipes ^ConfigData (.configData this)))
-(defn -setNumRSnipes [^Config this ^long newval] (reset! (:num-r-snipes ^ConfigData (.configData this)) newval))
+(defn -getInitialSnipeEnergy ^double [^SimConfig this] @(:initial-snipe-energy ^SimConfigData (.simConfigData this)))
+(defn -setInitialSnipeEnergy [^SimConfig this ^double newval] (reset! (:initial-snipe-energy ^SimConfigData (.simConfigData this)) newval))
+(defn -getInitialRSnipePriors [^SimConfig this] @(:initial-r-snipe-priors ^SimConfigData (.simConfigData this)))
+(defn -setInitialRSnipePriors [^SimConfig this newval] (reset! (:initial-r-snipe-priors ^SimConfigData (.simConfigData this) newval)))
+(defn -getNumKSnipes ^long [^SimConfig this] @(:num-k-snipes ^SimConfigData (.simConfigData this)))
+(defn -setNumKSnipes [^SimConfig this ^long newval] (reset! (:num-k-snipes ^SimConfigData (.simConfigData this)) newval))
+(defn -getNumRSnipes ^long [^SimConfig this] @(:num-r-snipes ^SimConfigData (.simConfigData this)))
+(defn -setNumRSnipes [^SimConfig this ^long newval] (reset! (:num-r-snipes ^SimConfigData (.simConfigData this)) newval))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -89,13 +89,13 @@
 
 (defn -main
   [& args]
-  (record-commandline-args! args) ; The Config isn't available yet, so store commandline args for later access by start().
-  (sim.engine.SimState/doLoop free-agent.Config (into-array String args))
+  (record-commandline-args! args) ; The SimConfig isn't available yet, so store commandline args for later access by start().
+  (sim.engine.SimState/doLoop free-agent.SimConfig (into-array String args))
   (System/exit 0))
 
-(defn set-config-data-from-commandline!
-  "Set fields in the Config's configData from parameters passed on the command line."
-  [^Config state cmdline]
+(defn set-sim-config-data-from-commandline!
+  "Set fields in the SimConfig's simConfigData from parameters passed on the command line."
+  [^SimConfig state cmdline]
   (let [{:keys [options arguments errors summary]} @cmdline]
     (when-let [newval (:energy options)] (.setEnergy state newval))
     (when-let [newval (:popsize options)] (.setNumKSnipes state newval) (.setNumRSnipes state newval)))
@@ -106,18 +106,18 @@
 
 (defn -start
   "Function called to (re)start a new simulation run."
-  [^Config this]
+  [^SimConfig this]
   (.superStart this)
   ;; If user passed commandline options, use them to set parameters, rather than defaults:
-  (when @commandline (set-config-data-from-commandline! this commandline))
+  (when @commandline (set-sim-config-data-from-commandline! this commandline))
   ;; Construct core data structures of the simulation:
   (let [^Schedule schedule (.schedule this)
-        ^ConfigData istate (.configData this)]
+        ^SimConfigData istate (.simConfigData this)]
     ;; Schedule per-tick step function(s):
     (.scheduleRepeating schedule Schedule/EPOCH 0
                         (reify Steppable 
                           (step [this sim-state]
-                            (let [^Config state sim-state]
+                            (let [^SimConfig state sim-state]
                               ;(doseq [^Indiv indiv population] (copy-relig! indiv state))      ; first communicate relig (to newrelig's)
                               ;(doseq [^Indiv indiv population] (update-relig! indiv))        ; then copy newrelig to relig ("parallel" update)
                               ;(doseq [^Indiv indiv population] (update-success! indiv state))  ; update each indiv's success field (uses relig)
