5a9e197d9ab4c5a560f89e74a16a11113a56c526
mars0i <mabrams@uab.edu>
2016-12-09 11:23:44 -0600

    old but potentially useful code goes here

---
+++ doc/notes/oldcode.clj
@@ -0,0 +1,9 @@
+
+
+(defn hyphed-to-camel-str
+  "Converts a hyphenated string into the corresponding camelcase string."
+  [hyphed-str]
+  (let [[first-part & other-parts] (s/split hyphed-str #"-")]
+    (reduce str 
+            first-part 
+            (map s/capitalize other-parts))))
