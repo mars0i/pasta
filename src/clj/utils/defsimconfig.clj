78654677ab709262e041e0b4db79f6220dfa7d86
mars0i <mabrams@uab.edu>
2016-12-10 13:40:26 -0600

    replaced local vars with gensyms in defsimconfig

--- src/clj/utils/defsimconfig.clj
+++ src/clj/utils/defsimconfig.clj
@@ -55,6 +55,7 @@
 
 ;; TODO add type annotations. (maybe iff they're symbols??)
 ;; TODO put data structure in its own namespace to avoid circular references
+;; Maybe some of gensym pound signs are overkill. Can't hurt.
 (defmacro defsimconfig
   "fields is a
   sequence of 2- or 4-element sequences starting with names of fields in which 
@@ -67,44 +68,44 @@
   before the last dot of the current namespace.  Java bean style and other MASON-style 
   accessors will be defined."
   [fields & addl-gen-class-opts]
-   (let [class-prefix (get-class-prefix *ns*)
-         qualified-class (symbol (str class-prefix "." class-sym))
-         field-syms (map first fields)
-         field-keywords (map keyword field-syms)
-         default-syms (map #(symbol (str "default-" %)) field-syms)
-         accessor-stubs (map hyphed-sym-to-studly-str field-syms)
-         get-syms  (map (partial prefix-sym "get") accessor-stubs)
-         set-syms  (map (partial prefix-sym "set") accessor-stubs)
-         -get-syms (map (partial prefix-sym "-") get-syms)
-         -set-syms (map (partial prefix-sym "-") set-syms)
-         range-fields (get-range-fields fields)
-         dom-syms  (map (comp (partial prefix-sym "dom") hyphed-sym-to-studly-str first)
-                        range-fields)
-         -dom-syms (map (partial prefix-sym "-") dom-syms)
-         dom-keywords (map keyword dom-syms)
-         ranges (map nnext range-fields)
-         gen-class-opts {:name qualified-class
+   (let [class-prefix# (get-class-prefix *ns*)
+         qualified-class# (symbol (str class-prefix# "." class-sym))
+         field-syms# (map first fields)
+         field-keywords# (map keyword field-syms#)
+         default-syms# (map #(symbol (str "default-" %)) field-syms#)
+         accessor-stubs# (map hyphed-sym-to-studly-str field-syms#)
+         get-syms#  (map (partial prefix-sym "get") accessor-stubs#)
+         set-syms#  (map (partial prefix-sym "set") accessor-stubs#)
+         -get-syms# (map (partial prefix-sym "-") get-syms#)
+         -set-syms# (map (partial prefix-sym "-") set-syms#)
+         range-fields# (get-range-fields fields)
+         dom-syms#  (map (comp (partial prefix-sym "dom") hyphed-sym-to-studly-str first)
+                        range-fields#)
+         -dom-syms# (map (partial prefix-sym "-") dom-syms#)
+         dom-keywords# (map keyword dom-syms#)
+         ranges# (map nnext range-fields#)
+         gen-class-opts# {:name qualified-class#
                          :extends 'sim.engine.SimState
                          :state data-sym
                          :exposes-methods '{start superStart}
                          :init init-genclass-sym
                          :main true
-                         :methods (vec (concat (make-accessor-sigs get-syms set-syms (map second fields))
-                                               (map #(vector % [] java.lang.Object) dom-syms)))} 
-         gen-class-opts (into gen-class-opts 
+                         :methods (vec (concat (make-accessor-sigs get-syms# set-syms# (map second fields))
+                                               (map #(vector % [] java.lang.Object) dom-syms#)))} 
+         gen-class-opts# (into gen-class-opts# 
                               (map vec (partition 2 addl-gen-class-opts)))]
      `(do
-        ;;;; should be in a different namespace (so simulation code can access it without cyclicly referencing SimConfig):
-        (defrecord ~data-class-sym ~(vec field-syms)) ; TODO make sure SimConfigData comes out in the right namespace
+        ;;;; TODO should be in a different namespace (so simulation code can access it without cyclicly referencing SimConfig):
+        (defrecord ~data-class-sym ~(vec field-syms#)) ; TODO make sure SimConfigData comes out in the right namespace
         ;;;; should be in SimConfig namespace:
-        (gen-class ~@(apply concat gen-class-opts))
-        (import ~qualified-class) ; must go after gen-class but before any type annotations using the class
+        (gen-class ~@(apply concat gen-class-opts#))
+        (import ~qualified-class#) ; must go after gen-class but before any type annotations using the class
         ;;;; Should be in same namespace as gen-class:
-        (defn ~init-defn-sym [~'seed] [[~'seed] (atom (~data-class-constructor ~@default-syms))]) ; NOTE will fail if default-syms are not yet defined.
+        (defn ~init-defn-sym [seed] [[seed] (atom (~data-class-constructor ~@default-syms#))]) ; NOTE will fail if default-syms are not yet defined.
         ;; TODO need to add type annotations:
-        ~@(map (fn [sym keyw] (list 'defn sym '[this] `(~keyw @(.simConfigData ~'this))))
-               -get-syms field-keywords)
-        ~@(map (fn [sym keyw] (list 'defn sym '[this newval] `(swap! (~data-accessor ~'this) assoc ~keyw  ~'newval)))
-               -set-syms field-keywords)
-        ~@(map (fn [sym keyw range-pair] (list 'defn sym '[this] `(Interval. ~@range-pair)))
-               -dom-syms dom-keywords ranges))))
+        ~@(map (fn [sym# keyw#] (list 'defn sym# '[this] `(~keyw# @(.simConfigData ~'this))))
+               -get-syms# field-keywords#)
+        ~@(map (fn [sym# keyw#] (list 'defn sym# '[this newval] `(swap! (~data-accessor ~'this) assoc ~keyw#  newval)))
+               -set-syms# field-keywords#)
+        ~@(map (fn [sym# keyw# range-pair#] (list 'defn sym# '[this] `(Interval. ~@range-pair#)))
+               -dom-syms# dom-keywords# ranges#))))
