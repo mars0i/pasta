48e31d1b244021be65b0c0cabd1ede267ca185b0
mabrams <mabrams@uab.edu>
2016-12-12 00:36:51 -0600

    revised make=snipe defs

--- src/clj/free_agent/snipe.clj
+++ src/clj/free_agent/snipe.clj
@@ -2,33 +2,22 @@
   (:require ;[free-agent.SimConfig :as cfg]
             [free-agent.level :as l]))
 
-(defrecord Snipe [id levels energy]) ; levels is a sequence of free-agent.Levels
-
-;; Needed??
-;(defprotocol SnipeP
-;  (getId [this])
-;  (getLevels [this])
-;  (getEnergy [this]))
-;(defrecord Snipe [id levels energy]
-;  SnipeP
-;  (getId [this] (:id this))
-;  (getLevels [this] (:levels this))
-;  (getEnergy [this] (:energy this)))
-
+;; The real difference between k- and r-snipes is in how levels is implemented,
+;; but it will be useful to have two different wrapper classes to make it easier to
+;; observe differences.
+(defrecord KSnipe [id levels energy]) ; levels is a sequence of free-agent.Levels
+(defrecord RSnipe [id levels energy]) ; levels is a sequence of free-agent.Levels
 
 ;; Could be a bottleneck if reproduction starts happening in different threads.
 ;; In that case I could switch to e.g. (str (gensym "i")) as in intermmitran.
 (def prev-snipe-id (atom -1)) ; first inc'ed value will be 0
 
-;; TODO revise to pass in config data from SimcConfig
-(defn make-k-snipe []
+(defn make-k-snipe [energy prior]
   (Snipe. (swap! prev-snipe-id inc) 
-          nil ;; TODO construct levels here using params from sim state (same for every k-snipe) (or pass in parent and clone it?)
-          (s/getInitialSnipeEnergy)))
+          nil ;; TODO construct levels here using prior
+          energy))
 
-(defn make-r-snipe [prior]
+(defn make-r-snipe [energy prior-0 prior-1]
   (Snipe. (swap! prev-snipe-id inc) 
-          nil ;; TODO construct levels here using prior (one of two values)
-          ;; note can't just clone parent since there's developmental variation
-          (s/getInitialSnipeEnergy)))
-
+          nil ;; TODO construct levels here using prior (one of two values, randomly)
+          energy))
