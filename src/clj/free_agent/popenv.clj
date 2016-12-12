b79caabf32d1a67eab0a17426e898920f8298cc5
mabrams <mabrams@uab.edu>
2016-12-12 13:57:39 -0600

    altered make-?-snipes to use entire cfg-data object

--- src/clj/free_agent/popenv.clj
+++ src/clj/free_agent/popenv.clj
@@ -6,22 +6,20 @@
 
 ;; need prior, energy, num snips
 (defn make-k-snipes
-  [num-snipes energy prior]
-  (repeatedly num-snipes #(sn/make-k-snipe energy prior)))
+  [cfg-data]
+  (let [{:keys [initial-energy k-snipe-prior num-k-snipes]} cfg-data]
+    (repeatedly num-k-snipes #(sn/make-k-snipe initial-energy k-snipe-prior))))
 
 (defn make-r-snipes
-  [num-snipes energy prior-0 prior-1]
-  (repeatedly num-snipes #(sn/make-r-snipe energy prior-0 prior-1)))
+  [cfg-data]
+  (let [{:keys [initial-energy r-snipe-prior-0 r-snipe-prior-1 num-r-snipes]} cfg-data]
+    (repeatedly num-r-snipes #(sn/make-r-snipe initial-energy r-snipe-prior-0 r-snipe-prior-1))))
 
 (defn make-popenv
   [cfg-data] ; TODO this is where circular ref will prevent type annotation if SimConfigData is defined in the SimConfig namespace
-  (let [{:keys [world-width world-height
-                initial-energy k-snipe-prior 
-               r-snipe-prior-0 r-snipe-prior-1 
-               num-k-snipes num-r-snipes]} cfg-data
-        k-snipes (make-k-snipes num-k-snipes initial-energy k-snipe-prior)
-        r-snipes (make-r-snipes num-r-snipes initial-energy 
-                                r-snipe-prior-0 r-snipe-prior-1)
+  (let [{:keys [world-width world-height]} cfg-data
+        k-snipes (make-k-snipes cfg-data)
+        r-snipes (make-r-snipes cfg-data)
         mushroom-field (DoubleGrid2D. world-width world-height -1.0) ; -1 means no mushroom.  eventually make two of these
         snipe-field    (ObjectGrid2D. world-width world-height)]     ; eventually make two of these
 
