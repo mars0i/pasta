1f2ba5afc5d3e57a71833bd5629aa782b7edbd57
mabrams <mabrams@uab.edu>
2016-12-12 23:04:21 -0600

    filled in make-popenv, added mushroom functions

--- src/clj/free_agent/popenv.clj
+++ src/clj/free_agent/popenv.clj
@@ -1,33 +1,56 @@
 (ns free-agent.popenv
   (:require ;[free-agent.SimConfig :as cfg]
             [free-agent.snipe :as sn]
-            [sim.field.grid DoubleGrid2D ObjectGrid2D]
-            [utils.random :as ran]))
+            [free-agent.mushroom :as mu]
+            [utils.random :as ran])
+  (:import [sim.field.grid ObjectGrid2D]))
 
 ;; need prior, energy, num snips
 (defn make-k-snipes
-  [cfg-data]
-  (let [{:keys [initial-energy k-snipe-prior num-k-snipes]} cfg-data]
-    (repeatedly num-k-snipes #(sn/make-k-snipe initial-energy k-snipe-prior))))
+  [cfg-data snipe-field]
+  (let [{:keys [world-width world-height initial-energy k-snipe-prior num-k-snipes]} cfg-data]
+    (repeatedly num-k-snipes #(sn/make-k-snipe initial-energy k-snipe-prior)))) ; TODO add them to field
+   ; TODO ADD SNIPES TO FIELD
 
 (defn make-r-snipes
-  [cfg-data]
-  (let [{:keys [initial-energy r-snipe-prior-0 r-snipe-prior-1 num-r-snipes]} cfg-data]
+  [cfg-data snipe-field]
+  (let [{:keys [world-width world-height initial-energy r-snipe-prior-0 r-snipe-prior-1 num-r-snipes]} cfg-data]
     (repeatedly num-r-snipes #(sn/make-r-snipe initial-energy r-snipe-prior-0 r-snipe-prior-1))))
+   ; TODO ADD SNIPES TO FIELD
+
+(defn maybe-add-mushroom
+  [rng mushroom-field x y mushroom-prob mean-0 mean-1 sd]
+  (when (< (ran/next-double rng) mushroom-prob)
+    (.set mushroom-field x y (mu/make-mushroom rng 
+                                               (if (< (ran/next-double rng) 0.5) mean-0 mean-1)
+                                               sd))))
+
+(defn make-mushrooms
+  "For each patch in mushroom-field, optionally add a new mushroom, with 
+  probability (:mushroom-prob cfg-data)."
+  [rng cfg-data mushroom-field]
+  (let [{:keys [world-width world-height mushroom-prob mushroom-mean-0 mushroom-mean-1 mushroom-sd]} cfg-data]
+    (doseq [x (range world-width)
+            y (range world-height)]
+      (maybe-add-mushroom rng mushroom-field x y 
+                          mushroom-mean-0 mushroom-mean-1 mushroom-sd))))
+
+(defrecord PopEnv [snipe-field mushroom-field])
 
 (defn make-popenv
-  [cfg-data] ; TODO this is where circular ref will prevent type annotation if SimConfigData is defined in the SimConfig namespace
+  [rng cfg-data]
   (let [{:keys [world-width world-height]} cfg-data
-        k-snipes (make-k-snipes cfg-data)
-        r-snipes (make-r-snipes cfg-data)
-        mushroom-field (DoubleGrid2D. world-width world-height -1.0) ; -1 means no mushroom.  eventually make two of these
-        snipe-field    (ObjectGrid2D. world-width world-height)]     ; eventually make two of these
+        snipe-field    (ObjectGrid2D. world-width world-height) ; eventually make two of each
+        mushroom-field (DoubleGrid2D. world-width world-height)]
+    (make-k-snipes cfg-data snipe-field)
+    (make-r-snipes cfg-data snipe-field)
+    (make-mushrooms rng cfg-data mushroom-field) ; no need for a separate list of mushrooms
+    (PopEnv. snipe-field mushroom-field)))
 
-
-    ))
-        
 
 (defn next-popenv
   [cfg-data prev-popenv]
+  ;; snipes move and/or eat
+  ;; mushrooms spawn
   )
 
