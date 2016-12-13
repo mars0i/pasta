814ea637d4b699cfd10e22ea4c5c23189e188183
mabrams <mabrams@uab.edu>
2016-12-12 22:55:49 -0600

    minor change

--- src/clj/free_agent/snipe.clj
+++ src/clj/free_agent/snipe.clj
@@ -2,15 +2,14 @@
   (:require ;[free-agent.SimConfig :as cfg]
             [free-agent.level :as l]))
 
+;; Could be a bottleneck if reproduction starts happening in different threads. In that case I could switch to e.g. (str (gensym "i")) as in intermmitran.
+(def prev-snipe-id (atom -1)) ; first inc'ed value will be 0
+
 ;; The real difference between k- and r-snipes is in how levels is implemented,
 ;; but it will be useful to have two different wrapper classes to make it easier to
 ;; observe differences.
 (defrecord KSnipe [id levels energy]) ; levels is a sequence of free-agent.Levels
 (defrecord RSnipe [id levels energy]) ; levels is a sequence of free-agent.Levels
-
-;; Could be a bottleneck if reproduction starts happening in different threads.
-;; In that case I could switch to e.g. (str (gensym "i")) as in intermmitran.
-(def prev-snipe-id (atom -1)) ; first inc'ed value will be 0
 
 (defn make-k-snipe [energy prior]
   (Snipe. (swap! prev-snipe-id inc) 
