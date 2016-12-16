fc9c3b35755b034851ceb3254fd03aa4aeea31d7
mars0i <mabrams@uab.edu>
2016-12-15 21:40:06 -0600

    now adds snipes to snipe-field

--- src/clj/free_agent/popenv.clj
+++ src/clj/free_agent/popenv.clj
@@ -5,34 +5,44 @@
             [utils.random :as ran])
   (:import [sim.field.grid ObjectGrid2D]))
 
-;; need prior, energy, num snips
-(defn make-k-snipes
-  [cfg-data snipe-field]
-  (let [{:keys [world-width world-height initial-energy k-snipe-prior num-k-snipes]} cfg-data]
-    (repeatedly num-k-snipes #(sn/make-k-snipe initial-energy k-snipe-prior)))) ; TODO add them to field
-   ; TODO ADD SNIPES TO FIELD
+(defn add-snipe
+  [rng field width height snipe]
+  (loop []
+    (let [x (ran/rand-idx rng width)
+          y (ran/rand-idx rng height)]
+      (if-not (.get field x y) ; empty slots contain Java nulls, i.e. Clojure nils
+        (.set field x y snipe)
+        (recur)))))
 
-(defn make-r-snipes
-  [cfg-data snipe-field]
+(defn add-k-snipes
+  [rng cfg-data field]
+  (let [{:keys [world-width world-height initial-energy k-snipe-prior num-k-snipes]} cfg-data]
+    (dotimes [_ (:num-k-snipes cfg-data)]
+      (add-snipe rng field world-width world-height 
+                 (sn/make-k-snipe initial-energy k-snipe-prior)))))
+
+(defn add-r-snipes
+  [rng cfg-data field]
   (let [{:keys [world-width world-height initial-energy r-snipe-prior-0 r-snipe-prior-1 num-r-snipes]} cfg-data]
-    (repeatedly num-r-snipes #(sn/make-r-snipe initial-energy r-snipe-prior-0 r-snipe-prior-1))))
-   ; TODO ADD SNIPES TO FIELD
+    (dotimes [_ num-r-snipes]
+      (add-snipe rng field world-width world-height 
+                 (sn/make-r-snipe initial-energy r-snipe-prior-0 r-snipe-prior-1)))))
 
 (defn maybe-add-mushroom
-  [rng mushroom-field x y mushroom-prob mean-0 mean-1 sd]
+  [rng field x y mushroom-prob mean-0 mean-1 sd]
   (when (< (ran/next-double rng) mushroom-prob)
-    (.set mushroom-field x y (mu/make-mushroom rng 
+    (.set field x y (mu/make-mushroom rng 
                                                (if (< (ran/next-double rng) 0.5) mean-0 mean-1)
                                                sd))))
 
-(defn make-mushrooms
+(defn add-mushrooms
   "For each patch in mushroom-field, optionally add a new mushroom, with 
   probability (:mushroom-prob cfg-data)."
-  [rng cfg-data mushroom-field]
+  [rng cfg-data field]
   (let [{:keys [world-width world-height mushroom-prob mushroom-mean-0 mushroom-mean-1 mushroom-sd]} cfg-data]
     (doseq [x (range world-width)
             y (range world-height)]
-      (maybe-add-mushroom rng mushroom-field x y mushroom-prob
+      (maybe-add-mushroom rng field x y mushroom-prob
                           mushroom-mean-0 mushroom-mean-1 mushroom-sd))))
 
 (defrecord PopEnv [snipe-field mushroom-field])
@@ -42,9 +52,9 @@
   (let [{:keys [world-width world-height]} cfg-data
         snipe-field    (ObjectGrid2D. world-width world-height) ; eventually make two of each
         mushroom-field (ObjectGrid2D. world-width world-height)]
-    (make-k-snipes cfg-data snipe-field)
-    (make-r-snipes cfg-data snipe-field)
-    (make-mushrooms rng cfg-data mushroom-field) ; no need for a separate list of mushrooms
+    (add-k-snipes rng cfg-data snipe-field)
+    (add-r-snipes rng cfg-data snipe-field)
+    (add-mushrooms rng cfg-data mushroom-field) ; no need for a separate list of mushrooms
     (PopEnv. snipe-field mushroom-field)))
 
 
