72de8292a6f1a138a83849dd71e62822b6fa5f79
mars0i <mabrams@uab.edu>
2016-12-17 12:22:37 -0600

    added :name to gen-class

--- src/clj/free_agent/mushroom.clj
+++ src/clj/free_agent/mushroom.clj
@@ -1,6 +1,7 @@
 (ns free-agent.mushroom
   (:require [utils.random :as ran])
-  (:gen-class)) ; so can be aot-compiled
+  (:gen-class                   ; so it can be aot-compiled
+    :name free-agent.mushroom)) ; without :name other aot classes won't find it
 
 ;; Could be a bottleneck if reproduction starts happening in different threads.  In that case I could switch to e.g. (str (gensym "i")) as in intermmitran.
 (def prev-mush-id (atom -1)) ; first inc'ed value will be 0
--- src/clj/free_agent/snipe.clj
+++ src/clj/free_agent/snipe.clj
@@ -1,6 +1,7 @@
 (ns free-agent.snipe
   (:require [free-agent.level :as l])
-  (:gen-class)) ; so it can be aot-compiled
+  (:gen-class                 ; so it can be aot-compiled
+     :name free-agent.snipe)) ; without :name other aot classes won't find it
 
 ;; Could be a bottleneck if reproduction starts happening in different threads. In that case I could switch to e.g. (str (gensym "i")) as in intermmitran.
 ;(def prev-snipe-id (atom -1)) ; first inc'ed value will be 0
