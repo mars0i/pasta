3db4104db9be55726019828f47be4acd96ee7b6d
mars0i <mabrams@uab.edu>
2016-12-20 22:55:38 -0600

    added clojure.algo.generic.functor

--- project.clj
+++ project.clj
@@ -8,6 +8,7 @@
                  [org.clojure/math.numeric-tower "0.0.4"]
                  [org.clojure/tools.cli "0.3.5"]
                  [org.clojure/data.csv "0.1.3"]
+                 [org.clojure/algo.generic "0.1.2"]
                  [net.mikera/core.matrix "0.57.0"]
                  [net.mikera/vectorz-clj "0.45.0"]
                  [incanter "1.5.7"]
