58a7e4ce15eda55352a1bf9e3f73aae68766e159
mars0i <mabrams@uab.edu>
2016-12-27 15:12:01 -0600

    added death, improved code organization

--- src/clj/free_agent/popenv.clj
+++ src/clj/free_agent/popenv.clj
@@ -10,14 +10,17 @@
 (declare setup-popenv! new-popenv make-popenv populate-env! organism-setter 
          add-organism-to-rand-loc!  add-k-snipes!  add-r-snipes! add-mush! 
          maybe-add-mush! add-mushs!  perceive-mushroom eat-if-appetizing snipes-eat 
-         choose-next-loc move-snipe! move-snipes next-popenv)
+         choose-next-loc move-snipe! move-snipes next-popenv snipes-die)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; TOP LEVEL FUNCTIONS
+
+(defrecord PopEnv [snipe-field mush-field])
 
 (defn setup-popenv-config!
   [cfg-data$]
   (let [{:keys [env-width]} @cfg-data$]
     (swap! cfg-data$ assoc :env-center (/ env-width 2.0))))
-
-(defrecord PopEnv [snipe-field mush-field])
 
 (defn make-popenv
   [rng cfg-data]
@@ -34,11 +37,13 @@
 (defn next-popenv
   [popenv rng cfg-data] ; put popenv first so we can swap! it
   (let [{:keys [snipe-field mush-field]} popenv
+        snipe-field (snipes-die cfg-data snipe-field)
         snipe-field (move-snipes rng cfg-data snipe-field)                         ; replaces with snipes with new snipes with new positions
         [snipe-field mush-field] (snipes-eat rng cfg-data snipe-field mush-field)] ; replaces snipes with new snipes with same positions
     (PopEnv. snipe-field mush-field)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; CREATE AND PLACE ORGANISMS
 
 (defn organism-setter
   ([organism-maker]
@@ -109,6 +114,67 @@
 ;      (when-let [snipe (.get snipe-field x y)]
 ;        (println (and (= x (:x snipe)) (= y (:y snipe))))))
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; MOVEMENT
+
+(defn move-snipes
+  [rng cfg-data snipe-field]
+  (let [{:keys [env-width env-height]} cfg-data
+        snipes (.elements snipe-field)
+        loc-snipe-vec-maps (for [snipe snipes  ; make seq of maps with a coord pair as key and singleton seq containing snipe as val
+                                 :let [next-loc (choose-next-loc rng snipe-field snipe)]] ; can be current loc
+                             next-loc)
+        loc-snipe-vec-map (apply merge-with concat loc-snipe-vec-maps) ; convert sec of maps to a single map where snipe-vecs with same loc are concatenated
+        loc-snipe-map (into {}                                       ; collect several maps into one
+                            (for [[coords snipes] loc-snipe-vec-map] ; go through key-value pairs, where values are collections containing one or more snipes
+                              (let [len (count snipes)]              ; convert to key-value pairs where value is a snipe
+                                (if (= len 1)
+                                  [coords (first snipes)]                          ; when more than one
+                                  (let [mover (nth snipes (ran/rand-idx rng len))] ; randomly select one to move
+                                    (into {coords mover} (map (fn [snipe] {[(:x snipe) (:y snipe)] snipe}) ; and make others "move" to current loc
+                                                              (remove #(= mover %) snipes))))))))         ; (could be more efficient to leave them alone)
+        new-snipe-field (ObjectGrid2D. env-width env-height)]
+    ;; Since we have a collection of all new snipe positions, including those
+    ;; who remained in place, we can just place them on a fresh snipe-field:
+    (doseq [[[x y] snipe] loc-snipe-map] ; will convert the map into a sequence of mapentries, which are seqs
+      (move-snipe! new-snipe-field x y snipe))
+    new-snipe-field))
+
+;; doesn't delete old snipe ref--designed to be used on an empty snipe-field:
+(defn move-snipe!
+  [snipe-field x y snipe]
+  (.set snipe-field x y (assoc snipe :x x :y y)))
+
+;; reusable bags for choose-next-loc
+(def x-coord-bag (IntBag. 6))
+(def y-coord-bag (IntBag. 6))
+
+(defn choose-next-loc
+  "Return a pair of field coordinates randomly selected from the empty 
+  hexagonally neighboring locations of snipe's location, or the current
+  location if all neighboring locations are filled."
+  [rng snipe-field snipe]
+  (let [curr-x (:x snipe)
+        curr-y (:y snipe)]
+    (.getHexagonalLocations snipe-field              ; inserts coords of neighbors into x-pos and y-pos args
+                            curr-x curr-y
+                            1 Grid2D/TOROIDAL false  ; immediate neighbors, toroidally, don't include me
+                            x-coord-bag y-coord-bag) ; will hold coords of neighbors
+    (let [candidate-locs (for [[x y] (map vector (.toIntegerArray x-coord-bag)  ; x-pos, y-pos have to be IntBags
+                                          (.toIntegerArray y-coord-bag)) ; but these are not ISeqs like Java arrays
+                               :when (not (.get snipe-field x y))] ; when cell is empty
+                           [x y])]
+      (if (seq candidate-locs) ; when not empty
+        (let [len (count candidate-locs)
+              idx (ran/rand-idx rng len)
+              [next-x next-y] (nth candidate-locs idx)]
+          {[next-x next-y] [snipe]}) ; key is a pair of coords; val is a single-element vector containing a snipe
+        {[curr-x curr-y] [snipe]}))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; PERCEPTION AND EATING
+
 (defn perceive-mushroom [snipe mush]
   [snipe true]) ; FIXME
 
@@ -117,7 +183,6 @@
   (max 0 
        (min max-energy 
             (+ snipe-energy mush-nutrition))))
-
 
 (defn eat-if-appetizing 
   "Returns a pair containing (a) a new version of the snipe, with 
@@ -154,60 +219,15 @@
                                      (partial add-mush! rng cfg-data))))) ; can't use same procedure for mushrooms as for snipes
     [new-snipe-field new-mush-field]))
 
-;; reusable bags for choose-next-loc
-(def x-coord-bag (IntBag. 6))
-(def y-coord-bag (IntBag. 6))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; BIRTH AND DEATH
 
-(defn choose-next-loc
-  "Return a pair of field coordinates randomly selected from the empty 
-  hexagonally neighboring locations of snipe's location, or the current
-  location if all neighboring locations are filled."
-  [rng snipe-field snipe]
-  (let [curr-x (:x snipe)
-        curr-y (:y snipe)]
-    (.getHexagonalLocations snipe-field              ; inserts coords of neighbors into x-pos and y-pos args
-                            curr-x curr-y
-                            1 Grid2D/TOROIDAL false  ; immediate neighbors, toroidally, don't include me
-                            x-coord-bag y-coord-bag) ; will hold coords of neighbors
-    (let [candidate-locs (for [[x y] (map vector (.toIntegerArray x-coord-bag)  ; x-pos, y-pos have to be IntBags
-                                          (.toIntegerArray y-coord-bag)) ; but these are not ISeqs like Java arrays
-                               :when (not (.get snipe-field x y))] ; when cell is empty
-                           [x y])]
-      (if (seq candidate-locs) ; when not empty
-        (let [len (count candidate-locs)
-              idx (ran/rand-idx rng len)
-              [next-x next-y] (nth candidate-locs idx)]
-          {[next-x next-y] [snipe]}) ; key is a pair of coords; val is a single-element vector containing a snipe
-        {[curr-x curr-y] [snipe]}))))
-
-;; doesn't delete old snipe ref--designed to be used only after clearing snipe-field
-(defn move-snipe!
-  [snipe-field x y snipe]
-  (.set snipe-field x y (assoc snipe :x x :y y)))
-
-(defn move-snipes
-  [rng cfg-data snipe-field]
+(defn snipes-die
+  "Returns a new snipe-field with zero-energy snipes removed."
+  [cfg-data snipe-field]
   (let [{:keys [env-width env-height]} cfg-data
-        snipes (.elements snipe-field)
-        loc-snipe-vec-maps (for [snipe snipes  ; make seq of maps with a coord pair as key and singleton seq containing snipe as val
-                                 :let [next-loc (choose-next-loc rng snipe-field snipe)]] ; can be current loc
-                             next-loc)
-        loc-snipe-vec-map (apply merge-with concat loc-snipe-vec-maps) ; convert sec of maps to a single map where snipe-vecs with same loc are concatenated
-        loc-snipe-map (into {}                                       ; collect several maps into one
-                            (for [[coords snipes] loc-snipe-vec-map] ; go through key-value pairs, where values are collections containing one or more snipes
-                              (let [len (count snipes)]              ; convert to key-value pairs where value is a snipe
-                                (if (= len 1)
-                                  [coords (first snipes)]                          ; when more than one
-                                  (let [mover (nth snipes (ran/rand-idx rng len))] ; randomly select one to move
-                                    (into {coords mover} (map (fn [snipe] {[(:x snipe) (:y snipe)] snipe}) ; and make others "move" to current loc
-                                                              (remove #(= mover %) snipes))))))))         ; (could be more efficient to leave them alone)
-        new-snipe-field (ObjectGrid2D. env-width env-height)]
-    ;; non-functional version:
-        ;new-snipe-field snipe-field]
-    ;(.clear snipe-field)
-    ;; Since we have a collection of all new snipe positions, including those
-    ;; who remained in place, we can just place them on a fresh snipe-field:
-    (doseq [[[x y] snipe] loc-snipe-map] ; will convert the map into a sequence of mapentries, which are seqs
-      (move-snipe! new-snipe-field x y snipe))
-    ;; FIXME Since we destructively modified snipe-field, we don't have to assoc it in to a new popenv (oh...)
+        new-snipe-field (ObjectGrid2D. env-width env-height)
+        live-snipes (remove #(<= (:energy %) 0) (.elements snipe-field))]
+    (doseq [snipe live-snipes]
+      (.set new-snipe-field (:x snipe) (:y snipe) snipe))
     new-snipe-field))
