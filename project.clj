42c4d4c083a53520f94a09b1450ec406148bd15e
mars0i <mabrams@uab.edu>
2016-12-30 02:07:55 -0600

    added snipe back to aot

--- project.clj
+++ project.clj
@@ -23,7 +23,7 @@
   :resource-paths ["lib/*"]
   :source-paths ["src/clj"]
   ;:main free-agent.SimConfig
-  :aot [free-agent.SimConfig free-agent.UI]
+  :aot [free-agent.snipe free-agent.SimConfig free-agent.UI]
   :profiles {:nogui {:main free-agent.SimConfig} ; execute this with 'lein with-profile nogui run'
              :gui   {:main free-agent.UI}})      ; execute this with 'lein with-profile gui run'
              
