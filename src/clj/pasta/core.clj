3163c570c774eed208b5eaee48ed2a5512e8b65d
mars0i <mabrams@uab.edu>
2017-03-07 09:07:35 -0600

    Moved require of pasta.UI back to ns form
    Giving up on idea of having an uberjar that doesn't load the
    JFrame/awt icon.  Uberjar is supposed to have everything in it,
    so it doesn't make sense to load UI conditionally.  It's there
    anyway.
    
    I can still run using the nogui script without it.
    
    (Maybe at some future date I'll make two separate uberjars, i.e.
    one without pasta.UI that just runs SimConfig.)

--- src/clj/pasta/core.clj
+++ src/clj/pasta/core.clj
@@ -1,21 +1,13 @@
 (ns pasta.core
-  (:require [pasta.SimConfig :as cfg]) ;[pasta.UI :as ui]
+  (:require [pasta.SimConfig :as cfg]
+            [pasta.UI :as ui])
   (:gen-class))
-
-;; also need a way to send false for -g so I can force nogui
 
 (defn -main
   [& args]
   (cfg/record-commandline-args! args) 
   (if (and args (not (:use-gui (:options @cfg/commandline$)))) ; if commandline options, default to no-gui unless use-gui is true
     (cfg/mein args)
-    (do
-      (require 'pasta.UI)
-      (pasta.UI/mein args)))) ; otherwise default to gui
+    (ui/mein args))) ; otherwise default to gui
 
-;(defn -main
-;  [& args]
-;  (cfg/record-commandline-args! args) 
-;  (if (and args (not (:use-gui (:options @cfg/commandline$)))) ; if commandline options, default to no-gui unless use-gui is true
-;    (cfg/mein args)
-;    (ui/mein args))) ; otherwise default to gui
+; (println (:use-gui (:options @cfg/commandline$)))
