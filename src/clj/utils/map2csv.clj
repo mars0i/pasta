cc5414ceddd08a9ba33cf8d2c64016c01d4cfb51
mars0i <mabrams@uab.edu>
2017-03-09 22:08:27 -0600

    added new map spitting functions

--- src/clj/utils/map2csv.clj
+++ src/clj/utils/map2csv.clj
@@ -12,13 +12,13 @@
   [k]
   (st/replace (name k) #"-" "_"))
 
-(defn make-column-name-row
+(defn label-row
   "Returns sorted column label strings made from keys in map m."
   [m]
   (map keyword-to-column-name
        (sort (keys m))))
 
-(defn make-data-row
+(defn data-row
   "Returns values from map m sorted to match column name order."
   [m]
   (keys
@@ -30,3 +30,33 @@
   [f rows & options]
    (with-open [w (apply io/writer f options)]
      (csv/write-csv w rows)))
+
+(defn spit-map
+  "Writes map m to file f with keys formatted by label-row and values formatted
+  by data-row.  Options are those that can be passed to spit or writer."
+  [f m & options]
+  (apply split-csv f 
+                   [(label-row m) (data-row m)]
+                   options))
+
+(defn spit-maps
+  "Assumes maps ms have the same keys.  After writing a label row formatted with
+  keys by label-row to file f, writes values from each map, one map per row 
+  formatted by data-row. Options are those that can be passed to spit or writer."
+  [f ms & options]
+  (apply split-csv f 
+                   (cons (label-row (first m))
+                         (map data-row m))
+                   options))
+
+(defn spit-map-seqs
+  "mss is a sequence of sequences of maps, where the maps at the same index in
+  each inner sequence have the same keys.  Labels formatted from keys by 
+  label-row will be made into a single sequence, and each data row will be
+  created from the values of the maps in each sequence in mss.  Options are 
+  those that can be passed to spit or writer."
+  [f mss & options]
+  (apply split-csv f 
+                   (cons (mapcat label-row (first ms))
+                         (mapcat data-row ms))
+                   options))
