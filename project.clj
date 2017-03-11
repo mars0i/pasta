d4fe753e57e04df1a0128e207205bcbfbb829f2f
mars0i <mabrams@uab.edu>
2017-03-11 13:04:54 -0600

    added specter

--- project.clj
+++ project.clj
@@ -9,6 +9,7 @@
                  [org.clojure/tools.cli "0.3.5"]
                  [org.clojure/data.csv "0.1.3"]
                  [org.clojure/algo.generic "0.1.2"]
+                 [com.rpl/specter "1.0.0"]
                  ;[incanter "1.5.7"]
                  ;[net.mikera/core.matrix "0.57.0"]
                  ;[net.mikera/vectorz-clj "0.45.0"]
