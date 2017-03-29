a459da62a9e2680c6a8f9af3e76696040eea8b12
mabrams <mabrams@uab.edu>
2017-03-29 12:35:29 -0500

    upped version to 1.0 since it's complete for purpose of presentations

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pasta "0.2.0-SNAPSHOT"
+(defproject pasta "1.0.0"
   :description "Agent-based simulation with free energy minimization within agents."
   :url "https://github.com/mars0i/pasta"
   :license {:name "Gnu General Public License version 3.0"
@@ -13,7 +13,7 @@
                  ;[incanter "1.5.7"]
                  ;[net.mikera/core.matrix "0.57.0"]
                  ;[net.mikera/vectorz-clj "0.45.0"]
-                 [criterium "0.4.4"] ; to use, e.g.: (use '[criterium.core :as c])
+                 ;[criterium "0.4.4"] ; to use, e.g.: (use '[criterium.core :as c])
                  [mason "19"] ; (slightly hacked version) can be installed in local maven repo: uncomment lein-localrepo below, run 'lein localrepo install lib/mason.19.noAddlApps.jar mason 19'
                  ;; Libs that MASON wants and can be gotten from maven.org, so they don't need to be in my lib dir:
                  [javax.media/jmf "2.1.1e"]
