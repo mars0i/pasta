08134735244693353b2792abb17245cf9cf185d6
mars0i <mabrams@uab.edu>
2019-01-23 10:28:47 -0600

    added bwplot variants

--- src/R/KvsR_birthcost75_negnut01.R
+++ src/R/KvsR_birthcost75_negnut01.R
@@ -11,3 +11,9 @@ you to filter on step as a numeric value.  Example:
 
 bwplot(count ~ snipe_class | as.factor(step), data=kvsr[kvsr$step>=550,], main="k-snipe, r-snipe pop sizes across 50 runs\nbirth cost = 7.5, neg nutrition = -0.1", xlab="snipe variety")
 
+# with hanoi, bar for median, red dot for mean:
+panel.mean <- function(x, y, ...) {
+    tmp <- tapply(y, x, FUN = mean);
+    panel.points(y=tmp, x=seq_along(tmp), ...)
+}
+bwplot(count ~ snipe_class | as.factor(step), data=yo, layout=c(10,3), panel=function(x,y,...){panel.bwplot(x,y, pch="|",...);panel.mean(x,y,pch=".", col="red", cex=5);panel.hanoi(x,y,...)}, main="k-snipe and r-snipe counts at different timesteps across 100 runs with default parameters", scales=list(alternating=c(3,3)))
