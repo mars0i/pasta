09b26ade28403178033614396c10fe5eed7448ba
mabrams <mabrams@uab.edu>
2019-03-25 01:19:03 -0500

    made main function smarter

--- src/R/makeCountTimeseriesPlots.R
+++ src/R/makeCountTimeseriesPlots.R
@@ -15,12 +15,14 @@ require(latticeExtra)
 #    makeCountlTimeseriesPlots(kvsr, c(10,5), "r", "K vs R")
 # Grayscale example with k-snipes lines as black (0.0) and dashed (2), r-snipe line solid (1) but gray (0.5):
 #    makeCountTimeseriesPlots(kvsr, c(10,5), "r", "K vs. R", colors=gray(c(0.5,0.0)), linetypes=c(1,2))
+# line types: 1: solid, 2: dashed, 3: dotted (there are other options)
 # Wrap this in pdf() to make a pdf
+# Set stripheight to 0 to get rid of the run id panel strips above each plot
 # 
 # What is altsnipes?  The function always plots k-snipes.  altsnipes specifies what
 # other kind of snipe to plot.
 
-makeCountTimeseriesPlots <- function(dframe, layoutdims, altsnipes, title, colors=c("red", "blue"), linetypes=c(1,1)){
+makeCountTimeseriesPlots <- function(dframe, layoutdims, altsnipes, title, colors=c("red", "blue"), linetypes=c(1,1), stripheight=1){
   # prepare the data:
   dframe$run <- factor(dframe$run)
   dframe.ag <- aggregate(count ~ snipe_class * run * step, dframe, sum)
@@ -38,7 +40,35 @@ makeCountTimeseriesPlots <- function(dframe, layoutdims, altsnipes, title, color
 				           space="bottom",    # where it is
 				           text=list(lab=c(paste(altsnipes, "-snipes", sep=""), "k-snipes")), # names of items
 			                   lines=list(col=colors, lty=linetypes, lwd=2)), # colors, line types, widths of example lines
-				  par.strip.text=list(cex=1.0)) # font size of panel header text (run numbers)
+				  par.strip.text=list(cex=1.0, lines=stripheight)) # font size of panel header text (run numbers)
+  # make second plot layer:
+  ksnipes <- xyplot(count ~ step | run, data=dframe.ag[dframe.ag$snipe_class=="k",], type=c("l"), lty=linetypes[2], lwd=2, col=colors[2])
+
+  # put two xyplots layers together into one:
+  altsnipes + as.layer(ksnipes) # do it this way rather than other way around because the first one sets the scale, and k is lower, in general
+}
+
+# special version with custom y axis limits built in
+hackyMakeCountTimeseriesPlots <- function(dframe, layoutdims, altsnipes, title, colors=c("red", "blue"), linetypes=c(1,1), stripheight=1){
+  # prepare the data:
+  dframe$run <- factor(dframe$run)
+  dframe.ag <- aggregate(count ~ snipe_class * run * step, dframe, sum)
+  # make first plot layer:
+  altsnipes <- xyplot(count ~ step | run, data=dframe.ag[dframe.ag$snipe_class==altsnipes,],
+                                  ylim=c(0,2000), # HACK
+                                  type="l",  # draw lines between points
+                                  lty=linetypes[1], # dashed, solid, etc.
+                                  lwd=2,  # line width
+                                  col=colors[1],    # color of line
+				  layout=layoutdims, # layout of panels in big plot window
+                                  cex=10, # font size I think
+				  main=title,        # main title
+				  xlab="timestep\n", ylab="number of snipes", # x, y labels
+				  key=list(columns=2,         # legend with two columns
+				           space="bottom",    # where it is
+				           text=list(lab=c(paste(altsnipes, "-snipes", sep=""), "k-snipes")), # names of items
+			                   lines=list(col=colors, lty=linetypes, lwd=2)), # colors, line types, widths of example lines
+				  par.strip.text=list(cex=1.0, lines=stripheight)) # font size of panel header text (run numbers)
   # make second plot layer:
   ksnipes <- xyplot(count ~ step | run, data=dframe.ag[dframe.ag$snipe_class=="k",], type=c("l"), lty=linetypes[2], lwd=2, col=colors[2])
 
