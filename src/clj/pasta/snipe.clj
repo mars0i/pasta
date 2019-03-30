26f87c26ec0564eb1d261fcd2830415af0a72719
mabrams <mabrams@uab.edu>
2019-03-30 00:21:53 -0500

    deleted old commented-out make-properties function

--- src/clj/pasta/snipe.clj
+++ src/clj/pasta/snipe.clj
@@ -170,57 +170,6 @@
   [rng cfg-data$ subenv new-id x y]
   (make-s-snipe rng cfg-data$ (rand-energy rng @cfg-data$) subenv new-id x y))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; MAKE-PROPERTIES FUNCTION
-
-;; Used by GUI to allow inspectors to follow a fnlly updated agent.
-;; (Code below makes use of the fact that in Clojure, vectors can be treated as functions
-;; of indexes, returning the indexed item; that keywords such as :x can be treated as 
-;; functions of maps; and that defrecords such as snipes can be treated as maps.)
-;(defn make-properties
-;  "Return a Properties subclass for use by Propertied's properties method so
-;  that certain fields can be displayed in the GUI on request."
-;  [id cfg-data$]
-;  ;; These definitions need to be coordinated by hand:
-;  (let [kys [:energy :mush-pref :subenv :x :y :age :lifespan :circled$] ; TODO CHANGE FOR NEW FIELDS
-;        circled-idx 7 ; HARDCODED INDEX for circled$ field              ; TODO CHANGE FOR NEW FIELDS
-;        descriptions ["Energy is what snipes get from mushrooms."       ; TODO CHANGE FOR NEW FIELDS
-;                      "Preference for large (positive number) or small (negative number) mushrooms."
-;                      "Name of snipe's subenv"
-;                      "x coordinate in underlying grid"
-;                      "y coordinate in underlying grid"
-;                      "Age of snipe"
-;                      "Maximum age"
-;                      "Boolean indicating whether circled in GUI"]
-;        types [java.lang.Double java.lang.Double java.lang.String java.lang.Integer java.lang.Integer ; TODO CHANGE FOR NEW FIELDS
-;               java.lang.Integer java.lang.Integer java.lang.Boolean]                                 ; TODO CHANGE FOR NEW FIELDS
-;        read-write [false false false false false false false true] ; allow user to turn off circled in UI ; TODO CHANGE FOR NEW FIELDS
-;        names (mapv name kys)
-;        num-properties (count kys)
-;        hidden     (vec (repeat num-properties false)) ; no properties specified here are to be hidden from GUI
-;        get-curr-snipe (fn [] ((:snipe-map (:popenv @cfg-data$)) id))] ; find current version of this snipe
-;    (reset! (:circled$ (get-curr-snipe)) true) ; make-properties is only called by inspector, in which case highlight snipe in UI
-;    (proxy [Properties] []
-;      (getObject [] (get-curr-snipe))
-;      (getName [i] (names i))
-;      (getDescription [i] (descriptions i))
-;      (getType [i] (types i))
-;      (getValue [i]
-;        (let [v ((kys i) (get-curr-snipe))]
-;          (cond (atom? v) @v
-;                (keyword? v) (name v)
-;                :else v)))
-;      (setValue [i newval]                  ; allow user to turn off circled in UI  ; POSS CHANGE FOR NEW FIELDS
-;        (when (= i circled-idx)             ; returns nil/null for other fields
-;          (reset! (:circled$ (get-curr-snipe))
-;                  (Boolean/valueOf newval)))) ; it's always a string that's returned from UI. (Do *not* use (Boolean. newval); it's always truthy in Clojure.)
-;      (isHidden [i] (hidden i))
-;      (isReadWrite [i] (read-write i))
-;      (isVolatile [] false)
-;      (numProperties [] num-properties)
-;      (toString [] (str "<SimpleProperties: snipe id=" id ">")))))
-
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; MISCELLANEOUS LITTLE FUNCTIONS
 
